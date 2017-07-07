ceccf36e259de6f2cc6ec0693031e66473899bdf
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2017-07-07 01:36:49 +0100

    fixing tests.

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -1,7 +1,12 @@
 'use strict';
 
-// var lib = require('../lib');
+var RC = require('../lib');
 
 describe('Main', function () {
-
+    it('must be able to load', function () {
+        var error = new TypeError('Invalid configuration object.');
+        expect(function () {
+            new RC();
+        }).toThrow(error);
+    });
 });
